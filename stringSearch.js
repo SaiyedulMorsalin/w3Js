659dc1598607c8448eb5ed06e41295361bfd9162
mdsaiyedul <mdsaiyedulmorsalin.code@gmail.com>
2023-08-29 00:44:29 +0600

    this w3 js series

---
+++ stringSearch.js
@@ -0,0 +1,12 @@
+// String Search Methods
+/*  1. Stting indexOf()
+    2. String lastIndxOf()
+    3. String search()
+    4. String matcha()
+    5. Sting matchAll()
+    6. String includes()
+    9. String startsWith()
+    10. String endsWith()
+
+
+*/
