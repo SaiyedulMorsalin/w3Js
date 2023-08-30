d1d43b5ae4d276fa9d3ebddd43721100a4b3b5ca
mdsaiyedul <mdsaiyedulmorsalin.code@gmail.com>
2023-08-31 02:27:46 +0600

    this

---
+++ arrayMethods.js
@@ -0,0 +1,21 @@
+// array Methods
+
+// converting array to string
+
+const fruits = ["Bannaba", "Orange", "Apple", "Mango"];
+const stringFruits = fruits.toString()
+// console.log(stringFruits);
+
+// Popping and Pushing Methods
+
+// pop removes an element from the last
+
+const popFruits = fruits.pop();
+console.log(popFruits);
+console.log(stringFruits);
+
+const pushFruits = fruits.push("Pinapple");
+console.log(fruits);
+
+const sFruits = fruits.shift()
+console.log(fruits);
--- numberDatatypes.js
+++ numberDatatypes.js
@@ -1,9 +1,22 @@
 // let n1 = 101;
 // // console.log(n1);
+//Integers (numbers without a period or exponent notation) are accurate up to 15 digits.
 //  let numlength = 999999999999999;
 // //  console.log(numlength);
 
   let ai = 0.4;
   let bi = 0.2;
   let sum = (ai * 10 + bi * 10)/10;
-  console.log(sum);
+//   console.log(sum);
+
+  let x = 123e5;  // exponent notation.
+  let y = 123e-5;
+  //console.log(x,y);
+
+  // JavaScript Numbers are Always 64-bit Floating Point
+/* 
+    WARNING !!
+    JavaScript uses the + operator for both addition and concatenation.
+    Numbers are added. Strings are concatenated.
+*/
+
