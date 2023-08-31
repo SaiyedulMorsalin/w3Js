13b88e14f589cc3a2aae77e66df026cfafef7060
mdsaiyedul <mdsaiyedulmorsalin.code@gmail.com>
2023-08-31 11:27:29 +0600

    day-2

--- arrayMethods.js
+++ arrayMethods.js
@@ -11,11 +11,43 @@ const stringFruits = fruits.toString()
 // pop removes an element from the last
 
 const popFruits = fruits.pop();
-console.log(popFruits);
-console.log(stringFruits);
+// console.log(popFruits);
+// console.log(stringFruits);
 
 const pushFruits = fruits.push("Pinapple");
-console.log(fruits);
+// console.log(fruits);
 
 const sFruits = fruits.shift()
-console.log(fruits);
+// console.log(fruits);
+
+const unFruits = fruits.unshift("Benji");
+// console.log(fruits);
+
+// console.log(delete fruits[0])
+// console.log(fruits);
+
+// // splice method 
+// console.log(fruits);
+const spliceFruits = fruits.splice(0, 0, "Pin")
+// console.log(fruits);
+fruits.splice(1, 1);
+// console.log(fruits);
+
+
+const fruits1 = ["Pinapple", "Banana"];
+const fruits2 = ["jackfruit", "Mango"];
+
+const fullFruits = fruits1.concat(fruits2);
+// console.log(fullFruits);
+
+const lastFruits = fullFruits.concat("Appleapple");
+// console.log(lastFruits);
+
+// slice method
+// slice method create a new array...
+ const sliceFruits = lastFruits.slice(3)
+//  console.log(sliceFruits);
+//  console.log(lastFruits);
+
+ // Autometic toString
+ document.getElementById("p").innerHTML = fruits;
---
+++ arraySort.js
@@ -0,0 +1,16 @@
+// sort method sort by alphabetical order
+
+const arr = ["Bannaba", "Orange", "Apple", "Mango"];
+const arrSort = arr.sort()
+console.log(arrSort.reverse());
+
+
+// sort a number of array
+
+
+const point  = [90, 230, 130, 540, 650, 960 ,40]
+point.sort((a, b) =>{
+    return b - a;
+});
+console.log(point);
+
