c28c175c4af930a1cdc3fb4db7a3f2399664bb33
mdsaiyedul <mdsaiyedulmorsalin.code@gmail.com>
2023-09-01 06:08:53 +0600

    day-3

--- booleans.js
+++ booleans.js
@@ -1,8 +1,8 @@
-console.log(Boolean("Check"));
+// console.log(Boolean("Check"));
 
 
-{
-    const a = 10;
-    const b = 6;
+// {
+//     const a = 10;
+//     const b = 6;
     
-}
+// }
--- w3js tutorial/output.js
+++ w3js tutorial/output.js
@@ -1,13 +1,13 @@
 // wrtitning into an HTML element using innerHTML
 // docment.write()
-const vowel=new Set(['a','e','i','o','u']);
-for(let x of s)
-{
-    if(vowel.has(x))
-    console.log(x);
-}
- for(let x of s)
-{
-    if(!vowel.has(x))
-    console.log(x);
-}
+// const vowel=new Set(['a','e','i','o','u']);
+// for(let x of s)
+// {
+//     if(vowel.has(x))
+//     console.log(x);
+// }
+//  for(let x of s)
+// {
+//     if(!vowel.has(x))
+//     console.log(x);
+// }
